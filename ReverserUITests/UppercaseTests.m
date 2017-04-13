e79e1520dc43897c24da7d0d1605e8ac1d6c822e
user <iOS-School-1@iMac-user-8.local>
2017-04-13 21:42:01 +0300

    UppercaseUITests

---
+++ ReverserUITests/UppercaseTests.m
@@ -0,0 +1,41 @@
+//
+//  UppercaseTests.m
+//  Reverser
+//
+//  Created by iOS-School-1 on 13.04.17.
+//  Copyright © 2017 Alexey Ulenkov. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface UppercaseTests : XCTestCase
+
+@end
+
+@implementation UppercaseTests
+
+- (void)setUp {
+    [super setUp];
+    
+}
+
+- (void)tearDown {
+    [super tearDown];
+}
+
+- (void)testExample {
+     NSString *reversedString = @"ABCD";
+    
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    XCUIElement *textField = [[app.otherElements containingType:XCUIElementTypeButton identifier:@"Button"] childrenMatchingType:XCUIElementTypeTextField].element;
+    [textField tap];
+    [textField typeText:@"abcd"];
+    [app.buttons[@"Button"] tap];
+    
+    XCUIElement *resultLabel = [app.staticTexts elementMatchingType:XCUIElementTypeAny identifier:reversedString];
+    
+    XCTAssertNotNil(resultLabel, @"Basic");
+    
+}
+
+@end
