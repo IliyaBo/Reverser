ecd785ab29c3c375417ab67af367bc07a31ab46c
user <iOS-School-1@iMac-user-8.local>
2017-04-13 21:44:32 +0300

    next commit

---
+++ Reverser/SBDUppercaser.h
@@ -0,0 +1,15 @@
+//
+//  SBDUppercaser.h
+//  Reverser
+//
+//  Created by iOS-School-1 on 13.04.17.
+//  Copyright © 2017 Alexey Ulenkov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SBDUppercaser : NSObject
+
++(NSString *) uppercase: (NSString *) string;
+
+@end
---
+++ Reverser/SBDUppercaser.m
@@ -0,0 +1,17 @@
+//
+//  SBDUppercaser.m
+//  Reverser
+//
+//  Created by iOS-School-1 on 13.04.17.
+//  Copyright © 2017 Alexey Ulenkov. All rights reserved.
+//
+
+#import "SBDUppercaser.h"
+
+@implementation SBDUppercaser
+
++(NSString *) uppercase: (NSString *) string{
+    return string.uppercaseString;
+}
+
+@end
